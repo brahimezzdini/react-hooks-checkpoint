015529c64986486407fda6b56af07612825529c5
brahimezzdini <80459078+brahimezzdini@users.noreply.github.com>
2021-07-07 13:21:22 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,8 +13,8 @@ function App() {
       <div className="App">
        <div className="container">
        <Switch>
-         <Route exact path="/" component={Home} />
-         <Route exact path="/movie/:id" component={MovieDetails} />
+         <Route exact path="/movies" component={Home} />
+         <Route exact path="/movies/:id" component={MovieDetails} />
              <Route exact component={Home} />
        </Switch>
        </div>
